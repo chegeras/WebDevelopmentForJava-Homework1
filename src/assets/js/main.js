02440bea6ab86a6275abbf7a2759100bb1b63911
chegeras <chegeras@gmail>
2014-08-01 02:27:46 +0400

    time info about item

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,23 +8,28 @@ jQuery(function($)
         var compiledTemplate = Handlebars.compile(template.html());
        $.getJSON(dataUrl, function(data) {
            //headingContainer.text(data.heading);
+
+           var date = new Date();
+           $.each(data.items,function(index,value)
+           {
+                value.time = "Today " + date.toTimeString().replace(/.*(\d{2}:\d{2}).*/, "$1");
+           });
            parentContainer.html('').append(compiledTemplate(data.items));
        }).fail(function( jqxhr, textStatus, error ) {
            var err = textStatus + ", " + error;
            console.log( "Request Failed: " + err );
        });
    }
-});
 
-function showProducts()
-{
     var template = $("#featured-products-list");
     var url = "assets/data/featured-products.json";
-    var parentContainer = $("#items-list");
-    $.showItems(template,url,parentContainer);
-}
+    var listContainer = $("#items-list");
+    $.showItems(template,url,listContainer);
 
-function showSearchResult()
-{
-    $("#featured-product-carousel").toggle("fast");
-}
+    $('#findBtn').on("click", function(){
+        $('#featured-product-carousel').slideUp('slow', function(){
+            $.showItems($('#search-products-list'), "assets/data/search-products.json",listContainer)
+        });
+        return false;
+    });
+});
